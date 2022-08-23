42b79b80e69525483b0b2a2459ed51439b39113c
meelis.utt <meelis.utt@gmail.com>
2022-08-23 22:09:40 +0300

    Fix/make better ex1-23

--- knr/ex1-23.c
+++ knr/ex1-23.c
@@ -2,65 +2,98 @@
 
 //Exercise 1-23. Write a program to remove all comments from a C program. Don't forget to
 //handle quoted strings and character constants properly. C comments don't nest.
+//Two cases: line comments and block comments
 
-// two cases: line comments and block comments
+// TODO: add whitespace storing(?)
 
-void line_comment();
-void block_comment();
+void line_comment(char b[2]);
+void block_comment(char b[2]);
+void quoted(char b[2]);
+void double_quoted(char b[2]);
+
+void line_comment(char buf[2])
+{
+	while ((buf[1]=getchar()) != EOF && buf[1] != '\n'){}
+	buf[0] = '\n';
+	buf[1] = getchar();
+}
+
+void block_comment(char buf[2])
+{
+	buf[0] = getchar();
+	while ((buf[1]=getchar()) != EOF ){
+		if (buf[0] == '*' && buf[1] == '/'){
+			break;
+		}
+		buf[0] = buf[1];
+	}
+	buf[0] = getchar();
+	buf[1] = getchar();
+}
+
+void double_quoted(char buf[2])
+{
+	putchar(buf[1]);
+	buf[0] = buf[1];
+	while ((buf[1] = getchar()) != EOF){
+		if (buf[1] == '"' && buf[0] != '\\'){
+			break;
+		}
+		putchar(buf[1]);
+		buf[0] = buf[1];
+	}
+	buf[0] = buf[1];
+	buf[1] = getchar();
+}
+
+void quoted(char buf[2])
+{
+	putchar(buf[1]);
+	while ((buf[1] = getchar()) != '\''){
+		putchar(buf[1]);
+	}
+	buf[0] = getchar();
+	if (buf[0] == '\''){
+		putchar(buf[0]);
+		buf[0] = getchar();
+	}
+}
 
 int main(void)
 {
-	int c;
-	int is_slash = 0;
-	while ((c=getchar()) != EOF){
-		if ( is_slash == 0 && c == '/' ){
-			is_slash = 1;
-			continue;
+	char s[21] = "\"testing /; //; /* */"; // TEST: double quotes
+	int test = 4/2;
+	char buf[2];
+	buf[0] = getchar();
+	while ((buf[1]=getchar()) != EOF){
+		if (buf[1] == '\''){
+			quoted(buf);
 		}
-		if (is_slash && c == '/' ){
-			line_comment();
-			is_slash = 0;
-			continue;
+		if (buf[1] == '\"'){
+			double_quoted(buf);
 		}
-		if (is_slash && c == '*' ){
-			block_comment();
-			is_slash = 0;
-			continue;
+		if (buf[0] == '/' && buf[1] == '/'){ // TEST: inline one-line comment
+			line_comment(buf);
+		}/*TEST:
+		inline 
+		block comment
+		FIXME:*/	if (buf[0] == '/' && buf[1] == '*'){
+			block_comment(buf);
 		}
-		if (is_slash && c != '/' && c != '*' ){
-			putchar('/');
-			is_slash = 0;
-		}
-		putchar(c);
+		putchar(buf[0]);
+		buf[0] = buf[1];
 	}
 	return 0;
 }
 
-void line_comment()
-{
-	int c;
-	while ((c=getchar()) != EOF && c != '\n')
-		;
-}
 
-/*
-test data: block comment
-*/void block_comment()
-{
-	int c;
-	int is_comment = 1;
-	int is_star = 0;
-	while ((c=getchar()) != EOF && is_comment){
-		if (is_star && c == '/')
-			is_comment = 1;
-		if ( c == '*' )
-			is_star = 1;
-	}
-}
 
-// test data: one line comment
+// TEST: line comment
+/* TEST: one line block comment */
 /*
-test data: block comment
+TEST: multi line
+TEST: block comment
 */
-/*test data: block comment*/
-
+// TEST: testing if division works in line comment: 4/2
+/*TEST: testing if division works in block comment: 4/2*/
+/* TEST: testing if division works in block comment: 4/2 */
